93d0b53dd4068dfdc61d5c207f005a75f55dbe37
helderthadeu <helderthadeubh@gmail.com>
2024-11-25 18:39:16 -0300

    Calendar Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
-import 'package:sqflite/sqflite.dart';
-import 'dart:async';
-
-import 'screens/settingsScreen.dart';
-import 'screens/calendarScreen.dart';
-import 'screens/deviceScreen.dart';
-import 'screens/homeScreen.dart';
-import 'screens/searchScreen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+import 'package:http/http.dart' show BaseRequest, Response;
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
   runApp(const MyApp());
-  // runApp(const DeviceScreen());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,74 +15,166 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Reminders App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePageWidget(),
-      // home: DeviceScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<HomePageWidget> createState() => _HomePageWidgetState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageWidgetState extends State<HomePageWidget> {
-  int _selectedItemPosition = 2;
+class _MyHomePageState extends State<MyHomePage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: "588397622893-v49pc7lmsgg49977932i4c9j80ro494u.apps.googleusercontent.com",
+    scopes: <String>[GoogleAPI.CalendarApi.calendarScope],
+  );
+
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        //getGoogleEventsData();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<List<GoogleAPI.Event>> getGoogleEventsData() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    final GoogleAPIClient httpClient =
+    GoogleAPIClient(await googleUser!.authHeaders);
+
+    final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+    final GoogleAPI.Events calEvents = await calendarApi.events.list(
+      "primary",
+    );
+    final List<GoogleAPI.Event> appointments = <GoogleAPI.Event>[];
+    if (calEvents.items != null) {
+      for (int i = 0; i < calEvents.items!.length; i++) {
+        final GoogleAPI.Event event = calEvents.items![i];
+        if (event.start == null) {
+          continue;
+        }
+        appointments.add(event);
+      }
+    }
+
+    return appointments;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: _buildBottomNavigationBar(),
-      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-      body: SafeArea(
-        child: _getBody(),
+      appBar: AppBar(
+        title: const Text('Event Calendar'),
+      ),
+      body: FutureBuilder(
+        future: getGoogleEventsData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Stack(
+            children: [
+              SfCalendar(
+                view: CalendarView.month,
+                initialDisplayDate: DateTime(2024, 11, 25, 9, 0, 0),
+                dataSource: GoogleDataSource(events: snapshot.data),
+                monthViewSettings: const MonthViewSettings(
+                    appointmentDisplayMode:
+                    MonthAppointmentDisplayMode.appointment),
+              ),
+              snapshot.data != null
+                  ? Container()
+                  : const Center(
+                child: CircularProgressIndicator(),
+              )
+            ],
+          );
+        },
       ),
     );
   }
 
-  Widget _buildBottomNavigationBar() {
-    return SnakeNavigationBar.color(
-      behaviour: SnakeBarBehaviour.floating,
-      snakeShape: SnakeShape.circle,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-      padding: const EdgeInsets.all(12),
-      snakeViewColor: Colors.black,
-      selectedItemColor: const Color.fromARGB(255, 223, 219, 219),
-      unselectedItemColor: Colors.blueGrey,
-      currentIndex: _selectedItemPosition,
-      onTap: (index) {
-        setState(() => _selectedItemPosition = index);
-      },
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-        BottomNavigationBarItem(icon: Icon(Icons.calendar_month), label: 'Calendar'),
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.devices), label: 'Devices'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-      ],
-    );
-  }
-
-  Widget _getBody() {
-    switch (_selectedItemPosition) {
-      case 0:
-        return const SettingsScreen();
-      case 1:
-        return const CalendarScreen();
-      case 2:
-        return const HomeScreen();
-      case 3:
-        return const DeviceScreen();
-      case 4:
-        return const SearchScreen();
-      default:
-        return const HomeScreen();
+  @override
+  void dispose() {
+    if (_googleSignIn.currentUser != null) {
+      _googleSignIn.disconnect();
+      _googleSignIn.signOut();
     }
+
+    super.dispose();
+  }
+}
+
+class GoogleDataSource extends CalendarDataSource {
+  GoogleDataSource({required List<GoogleAPI.Event>? events}) {
+    appointments = events;
   }
 
+  @override
+  DateTime getStartTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.start?.date ?? event.start!.dateTime!.toLocal();
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].start.date != null;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.endTimeUnspecified != null && event.endTimeUnspecified!
+        ? (event.start?.date ?? event.start!.dateTime!.toLocal())
+        : (event.end?.date != null
+        ? event.end!.date!.add(const Duration(days: -1))
+        : event.end!.dateTime!.toLocal());
+  }
+
+  @override
+  String getLocation(int index) {
+    return appointments![index].location ?? '';
+  }
+
+  @override
+  String getNotes(int index) {
+    return appointments![index].description ?? '';
+  }
+
+  @override
+  String getSubject(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.summary == null || event.summary!.isEmpty
+        ? 'No Title'
+        : event.summary!;
+  }
+}
+
+class GoogleAPIClient extends IOClient {
+  final Map<String, String> _headers;
+
+  GoogleAPIClient(this._headers) : super();
+
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+      super.head(url,
+          headers: (headers != null ? (headers..addAll(_headers)) : headers));
 }
